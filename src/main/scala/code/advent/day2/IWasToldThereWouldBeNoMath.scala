98b3ff223e7d69bc728be9b7933689c4656deb40
MarekKomaisko <78044249+marekk1992@users.noreply.github.com>
2023-12-09 20:53:56 +0200

    complete day2 (#2)
    * complete part1 of day2
    
    * complete part2 of day2

---
+++ src/main/scala/code/advent/day2/IWasToldThereWouldBeNoMath.scala
@@ -0,0 +1,34 @@
+package code.advent.day2
+
+import scala.io.Source
+
+object IWasToldThereWouldBeNoMath extends App {
+
+  private val bufferedSource = Source.fromFile("src/main/resources/inputs/adventofcode.com_2015_day_2_input.txt")
+
+  val result = (for {
+    rectangle <- bufferedSource.getLines().map(inputLine => Rectangle(inputLine)).toList
+  smallestPaperArea = rectangle.calculateRibbonAmount()
+  } yield smallestPaperArea).sum
+
+  println(result)
+
+  bufferedSource.close
+
+  class Rectangle(length: Int, width: Int, height: Int) {
+    private val dimensions = List(length, width, height)
+
+    def calculateRibbonAmount(): Int = {
+      val bowArea = length * width * height
+      val presentArea = dimensions.sorted.take(2).map(_*2).sum
+      bowArea + presentArea
+    }
+  }
+
+  private object Rectangle {
+    def apply(input: String): Rectangle = {
+      val dimensions = input.split('x')
+      new Rectangle(dimensions(0).toInt, dimensions(1).toInt, dimensions(2).toInt)
+    }
+  }
+}
