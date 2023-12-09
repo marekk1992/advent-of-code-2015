96ac2c5b54b93ec583ef941766c40d328382b0e5
MarekKomaisko <78044249+marekk1992@users.noreply.github.com>
2023-12-09 19:07:56 +0200

    complete day 1 (#1)
    * complete part1
    
    * complete part2

---
+++ src/main/scala/code/advent/day1/NotQuiteLisp.scala
@@ -0,0 +1,25 @@
+package code.advent.day1
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object NotQuiteLisp extends App {
+
+  private val bufferedSource = Source.fromFile("src/main/resources/inputs/adventofcode.com_2015_day_1_input.txt")
+  private val input = bufferedSource.getLines().mkString.zipWithIndex.map( { case (char, index) => index + 1 -> char }).toList
+
+  private val result = iterateFloors(0, 1, input)
+  println(result)
+
+  bufferedSource.close
+
+  @tailrec
+  private def iterateFloors(acc: Int, currentFloorIndex: Int, input: List[(Int, Char)]): Int = {
+    if (acc < 0) currentFloorIndex
+    else {
+      val currentFloor = input.head
+      val floor = if (currentFloor._2 == '(') 1 else -1
+      iterateFloors(acc + floor, currentFloor._1, input.drop(1))
+    }
+  }
+}
