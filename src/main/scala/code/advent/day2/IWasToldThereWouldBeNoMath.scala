46026f6ace7cca96b3fe86911d0e2c596ec5f496
marek1992 <m.komaisko@gmail.com>
2023-12-09 20:53:01 +0200

    complete part2 of day2

--- src/main/scala/code/advent/day2/IWasToldThereWouldBeNoMath.scala
+++ src/main/scala/code/advent/day2/IWasToldThereWouldBeNoMath.scala
@@ -4,32 +4,31 @@ import scala.io.Source
 
 object IWasToldThereWouldBeNoMath extends App {
 
-  case class Rectangle(length: Int, width: Int, height: Int)
-
-  private object Rectangle {
-    def createInstance(input: String): Rectangle = {
-      val dimensions = input.split('x')
-      Rectangle(dimensions(0).toInt, dimensions(1).toInt, dimensions(2).toInt)
-    }
-
-    def wrappingPaperArea(rectangle: Rectangle): Int = {
-      val sides = Seq(
-        rectangle.length * rectangle.height,
-        rectangle.height * rectangle.width,
-        rectangle.width * rectangle.length
-      )
-      sides.map(side => 2 * side).sum + sides.min
-    }
-  }
-
   private val bufferedSource = Source.fromFile("src/main/resources/inputs/adventofcode.com_2015_day_2_input.txt")
 
   val result = (for {
-    rectangle <- bufferedSource.getLines().map(inputLine => Rectangle.createInstance(inputLine)).toList
-    smallestPaperArea = Rectangle.wrappingPaperArea(rectangle)
+    rectangle <- bufferedSource.getLines().map(inputLine => Rectangle(inputLine)).toList
+  smallestPaperArea = rectangle.calculateRibbonAmount()
   } yield smallestPaperArea).sum
 
   println(result)
 
   bufferedSource.close
+
+  class Rectangle(length: Int, width: Int, height: Int) {
+    private val dimensions = List(length, width, height)
+
+    def calculateRibbonAmount(): Int = {
+      val bowArea = length * width * height
+      val presentArea = dimensions.sorted.take(2).map(_*2).sum
+      bowArea + presentArea
+    }
+  }
+
+  private object Rectangle {
+    def apply(input: String): Rectangle = {
+      val dimensions = input.split('x')
+      new Rectangle(dimensions(0).toInt, dimensions(1).toInt, dimensions(2).toInt)
+    }
+  }
 }
