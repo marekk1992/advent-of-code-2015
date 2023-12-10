dec74ccf88d65e3ab10f389c12795ac323a779bf
MarekKomaisko <78044249+marekk1992@users.noreply.github.com>
2023-12-10 15:01:39 +0200

    complete day 4 (#4)
    * complete part3 of day4
    
    * complete part2 of day4

---
+++ src/main/scala/code/advent/day4/TheIdealStockingStuffer.scala
@@ -0,0 +1,25 @@
+package code.advent.day4
+
+import java.security.MessageDigest
+import scala.annotation.tailrec
+
+object TheIdealStockingStuffer extends App {
+
+  val INPUT = "iwrupvqb"
+
+  val result = findLowestNumber(0, INPUT)
+  println(result)
+
+  @tailrec
+  private def findLowestNumber(number: Int, input: String): Int = {
+    if (startsWithFiveZeros(input + number)) number
+    else findLowestNumber(number + 1, input)
+  }
+
+  def startsWithFiveZeros(input: String): Boolean = {
+    val hexBytes = MessageDigest.getInstance("MD5").digest(input.getBytes)
+    val hexString = hexBytes.map("%02x".format(_)).mkString
+
+    hexString.take(6) == "000000"
+  }
+}
